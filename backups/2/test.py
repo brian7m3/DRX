181b1ee7d0d58cc471f1014a3116516e7760ed52
Brian F <email+github@sparc.us>
2025-07-05 16:45:14 -0400

    Remove all files from the repository

--- backups/2/test.py
+++
@@ -1,5 +0,0 @@
-import time
-from drx import is_cos_active
-while True:
-    print("COS:", is_cos_active())
-    time.sleep(0.2)
--- backups/old-backups/run_drx_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/brian/DRX
-/usr/bin/python3 /home/brian/DRX/drx_main.py > /home/brian/DRX/drx_main_direct.log 2>&1
